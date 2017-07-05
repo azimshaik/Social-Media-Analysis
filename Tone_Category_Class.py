4007e62ef7a046234574bbf18c0490901ccd7530
azimshaik <cheer.buddu@gmail.com>
2017-07-04 20:59:32 -0400

    cleaned up

--- Tone_Category_Class.py
+++ Tone_Category_Class.py
@@ -4,8 +4,6 @@ class Tone():
         self.tone_name = tone_name
         self.score = score
         self.tone_id = tone_id
-#Array of tones 
-toneArray = []
 
 class ToneCategory():
     def __init__(self, category_id, toneArray, category_name):
@@ -13,8 +11,4 @@ class ToneCategory():
         self.tones = toneArray
         self.category_name = category_name
 
-toneCategoryArray = []
 
-toneArray.append(Tone('Anger',0.80,'anger'))
-toneArray.append(Tone('Happy',0.90,'happy'))
-toneCategoryArray.append(ToneCategory('emotion_tone',toneArray,'Emotion Tone'))
